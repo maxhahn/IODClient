9dedb30a882570e459d0f36bc4eb988f421dbe1d
maxhahn <max.hahn@gmx.de>
2025-05-09 13:09:37 +0000

    started single pag sim

---
+++ test_graph_repeatedly.py
@@ -0,0 +1,12 @@
+
+
+"""
+1. Get data for 2 clients (maybe 4?)
+2. test locally
+3. test with fishers method
+4. test with fedci
+5. compare fisher and fedci <- if there are any differences in CI results, highlight the file
+
+6. run FCI and IOD on generated data -> compare graphs / compare SHD
+
+"""
